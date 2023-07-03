1498fd6507736f6b8a8a9213d048beec79d61907
Martin Pecka <peckama2@fel.cvut.cz>
2023-07-03 15:08:59 +0200

    Fixed build

--- electronic_io/setup.py
+++ electronic_io/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['electronic_io'],
+    packages=['electronic_io', 'electronic_io.devices', 'electronic_io.virtual_pins'],
     package_dir={'': 'src'}
 )
 
